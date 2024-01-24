3cd3edbb7f88f5e7132706a0a855ea094a068c8e
Joy Ghosh <145364240+ghoshjoy347@users.noreply.github.com>
2024-01-24 21:28:19 +0000

    basic  of react

--- 01_basic-react/src/App.js
+++ 01_basic-react/src/App.js
@@ -1,7 +1,12 @@
+import Joy from "./Joy"
+
 function App() {
   return (
+    <>
+    <Joy />
     <h1>Lets Begin React</h1>
+    </>
   );
 }
 
-export default App;
+export default App
---
+++ 01_basic-react/src/joy.js
@@ -0,0 +1,7 @@
+function joy(){
+    return(
+        <h2>code with joy</h2>
+    )
+}
+
+export default joy
