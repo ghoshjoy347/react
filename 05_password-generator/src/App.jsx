c3f39eac08ff920c477fae115781bd764033795d
Joy Ghosh <145364240+ghoshjoy347@users.noreply.github.com>
2024-01-28 17:00:25 +0000

    project-password-generator

--- 05_password-generator/src/App.jsx
+++ 05_password-generator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState,useCallback } from 'react'
+import { useState,useCallback,useEffect,useRef} from 'react'
 import './App.css'
 
 function App() {
@@ -8,6 +8,9 @@ function App() {
   const [charAllowed, setcharAllowed] = useState(false)
   const [password, setPassword] = useState("")
 
+  //useRef hook
+  const passwordRef = useRef(null)
+
   const passwordGenerator = useCallback(() => {
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -15,21 +18,27 @@ function App() {
     if (numberAllowed) str += "0123456789"
     if (charAllowed) str += "!~@#$%^&*{}[]"
 
-for (let i = 1; i <= array.length; i++){
+for (let i = 1; i <= length; i++){
   let char = Math.floor(Math.random() * str.length + 1)
-  pass = str.charAt(char)
+  pass += str.charAt(char)
 }
   setPassword(pass)
 
-  },[length,numberAllowed,charAllowed,setPassword])
+  }, [length,numberAllowed,charAllowed,setPassword])  // we can exclude setPassword, it is mostly used to optimise
 
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select() //? is for optinally select, cos current value can be 0
+    passwordRef.current?.setSelectionRange(0,99)  // we can include this or may not, but this is only to used to select the value in the desired range
+    window.navigator.clipboard.writeText(password)  // we can use "window" object, directly cos we are directly using React, but during usage in Next Js we have to use it in server side rendering, in Next Js the code is executed in server side and no window object
+  },[password])
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, numberAllowed,charAllowed,passwordGenerator])
   return (
     
-      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-8 text-orange-500 bg-gray-700'>
-        
-      <h1 className='text-white text-center my-3'>Password Generator</h1>
-
-      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+    <h1 className='text-white text-center my-3'>Password generator</h1>
+  <div className="flex shadow rounded-lg overflow-hidden mb-4">
 
     <input 
     type="text" 
@@ -37,9 +46,12 @@ for (let i = 1; i <= array.length; i++){
     className='outline-none w-full py-1 px-3'
     placeholder="Password"
     readOnly
+    ref={passwordRef}
     />
 
-    <button className='outline-none bg-blue-800 text-white px-3 py-1 shrink-0'>copy</button>
+    <button 
+    onClick={copyPasswordToClipboard}
+    className='outline-none bg-blue-800 text-white px-3 py-0.5 shrink-0'>copy</button>
 
       </div>
 
@@ -51,13 +63,13 @@ for (let i = 1; i <= array.length; i++){
           max={100}
           value={length}
           className='cursor-pointer'
-          onChange={(e)}
+          onChange={(e) => {setLength(e.target.value)}}
      />
           <label>Length: {length}</label>
     </div>
     <div className='flex items-center gap-x-1'>
       <input 
-        type="checkbbox"
+        type="checkbox"
         defaultChecked={numberAllowed}
         id="numberInput"
         onChange={() => {
@@ -69,7 +81,7 @@ for (let i = 1; i <= array.length; i++){
 
      <div className='flex items-center gap-x-1'>
       <input 
-        type="checkbbox"
+        type="checkbox"
         defaultChecked={charAllowed}
         id="characterInput"
         onChange={() => {
