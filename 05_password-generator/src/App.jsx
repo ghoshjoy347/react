6cfd55c76cafeee33626b708e7df5a9a0bebe7e6
Joy Ghosh <145364240+ghoshjoy347@users.noreply.github.com>
2024-01-28 22:23:17 +0000

    project_2-password-generator

--- 05_password-generator/src/App.jsx
+++ 05_password-generator/src/App.jsx
@@ -2,7 +2,7 @@ import { useState,useCallback,useEffect,useRef} from 'react'
 import './App.css'
 
 function App() {
-
+ 
   const [length, setLength] = useState(8)
   const [numberAllowed, setnumberAllowed] = useState(false)
   const [charAllowed, setcharAllowed] = useState(false)
