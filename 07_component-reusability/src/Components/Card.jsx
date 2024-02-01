1112da22dc8345f0fd7d7bb9e54fca754175041f
Joy Ghosh <145364240+ghoshjoy347@users.noreply.github.com>
2024-02-01 16:50:11 +0000

    conditional rendering

--- 07_component-reusability/src/Components/Card.jsx
+++ 07_component-reusability/src/Components/Card.jsx
@@ -3,9 +3,21 @@ import React from 'react'
 function Card() {
 
   const data = [
-    {image:'https://images.unsplash.com/photo-1633174524827-db00a6b7bc74?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YW1hem9ufGVufDB8fDB8fHww' , name: "Amazon Basics", description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, ducimus. Unde, expedita!"},
-    {image:'https://images.unsplash.com/photo-1624521793559-136bfe16fc86?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGFtYXpvbnxlbnwwfHwwfHx8MA%3D%3D' , name: "Daily Objects", description: "Lorem ipsum jgig, sit adipisicing elit. Amet, ducimus. Unde, expedita!"},
-    {image:'https://images.unsplash.com/photo-1586528116311-ad8dd3c8310d?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGFtYXpvbnxlbnwwfHwwfHx8MA%3D%3D' , name: "Apple", description: "Lorem ipsum dolor, sietrgr gconsectetur adipisicing elit. Amet, ducimus. Unde, expedita!"}
+    {image:'https://images.unsplash.com/photo-1633174524827-db00a6b7bc74?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YW1hem9ufGVufDB8fDB8fHww' , 
+    name: "Amazon Basics", 
+    description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet, ducimus. Unde, expedita!",
+    instock: true
+  },
+    {image:'https://images.unsplash.com/photo-1624521793559-136bfe16fc86?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGFtYXpvbnxlbnwwfHwwfHx8MA%3D%3D' , 
+    name: "Daily Objects", 
+    description: "Lorem ipsum jgig, sit adipisicing elit. Amet, ducimus. Unde, expedita kjef ijfn kry yyrj!",
+    instock: false
+  },
+    {image:'https://images.unsplash.com/photo-1586528116311-ad8dd3c8310d?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGFtYXpvbnxlbnwwfHwwfHx8MA%3D%3D' , 
+    name: "Apple", 
+    description: "Lorem ipsum dolor, sietrgr gconsectetur adipisicing elit. Amet, ducimus. Unde, expedita!",
+    instock: true
+  }
   ]
 
 
@@ -19,6 +31,7 @@ function Card() {
           <div className='w-full px-3 py-4'>
             <h2 className='font-semibold'>{elem.name}</h2>
             <p className='text-xs mt-3'>{elem.description}</p>
+            <button className={`px-4 py-2 ${elem.instock ? 'bg-sky-500' : 'bg-red-500'} text-xs rounded-md text-zinc-100 mt-3`}>{elem.instock ? "In Stock" : "Out of Stock"}</button>
         </div>
       </div>
       ))}
