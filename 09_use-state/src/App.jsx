948672e71d5ef911941fcb31060ef38fe51d6c16
Joy Ghosh <145364240+ghoshjoy347@users.noreply.github.com>
2024-02-02 17:54:44 +0000

    using use-state

--- 09_use-state/src/App.jsx
+++ 09_use-state/src/App.jsx
@@ -44,20 +44,96 @@ function App(){
   //   </div>
 
 
+//<--------------------------------------for true its blue and for false its red--------------------------------------------------------------->
+
+  // const [val,setVal] = useState({name:"joy", isLoggedIn: false})
+
+  // return (
+
+  //   <div className="p-4">
+
+  //     <h1>name:{val.name}</h1>
+  //     <h2>isLoggedIn:{val.isLoggedIn.toString()}</h2>                     {/*boolean values doesnt get printed, so it needs to convert to string*/}
+  //     <button onClick={()=>setVal({...val, isLoggedIn: !val.isLoggedIn})} className={`mt-2 px-3 py-1 ${val.isLoggedIn ? "bg-blue-400" : "bg-red-400"} rounded-full text-white`}>Change</button>
+
+  //   </div>
+
+  //<-----------------------------------------to update the field, but it won't update immediately after updating-------------------------------------------------------------------------------->
+  
+//   const [val,setVal] = useState({name:"joy", age: 22})
+
+//   return (
+
+//     <div>
+//       <button onClick={() => setVal({...val,gender: "male"})}>Click</button>    {/*it would update but not right after the fieid is updated*/}
+//     </div>
+  
+//   )
+// }
+
+//<--------------------------------------------removing the last element on click---------------------------------------------------------------------------------------->
+
+// const [val,setVal] = useState([1,2,3,4,5,6])
+
+//   return (
+
+//     <div>
+//       {val.map(item=><h1 className="ml-8">{item}</h1>)}
+//       <button onClick={()=>setVal(()=>{
+//         return val.filter((item,index) => index!=val.length-1)
+//       })}className="px-2 py-1 ml-4 mt-2 bg-blue-400 rounded-full text-xs text-white">Click</button>
+//     </div>
+  
+
+//<------------------------------------------------removing divisible by 2 from the array---------------------------------------------------------------------------->
 
 
-  const [val,setVal] = useState({name:"joy", isLoggedIn: false})
+// const [val,setVal] = useState([1,2,3,4,5,6])
+
+//   return (
+
+//     <div className="p-5">
+//       {val.map(item=><h1>{item}</h1>)}
+//       <button onClick={()=>setVal(()=>val.filter((item,index)=> item%2 !== 0))}className="px-2 py-1 ml-4 mt-2 bg-blue-400 rounded-full text-xs text-white">Click</button>
+//     </div>
+
+//   )
+// }
+
+//<----------------------------------------------------add element at the end of an array---------------------------------------------------------------->
+
+// const [val,setVal] = useState([1,2,3,4,5,6])
+
+//   return (
+
+//     <div className="p-5">
+//       {val.map(item=><h1>{item}</h1>)}
+//       <button onClick={()=>setVal([...val,7])} className="px-2 py-1 ml-4 mt-2 bg-blue-400 rounded-full text-xs text-white">Click</button>
+//     </div>
+
+//   )
+// }
+
+//<----------------------------------------------------add element at last---------------------------------------------------------------->
+
+const [val,setVal] = useState([
+  {name:"joy", age: 22},
+  {name:"messi", age: 25},
+  {name:"virat", age: 35},
+])
 
   return (
 
-    <div className="p-4">
-
-      <h1>name:{val.name}</h1>
-      <h2>isLoggedIn:{val.isLoggedIn.toString()}</h2>                     {/*boolean values doesnt get printed, so it needs to convert to string*/}
-      <button onClick={()=>setVal({...val, isLoggedIn: !val.isLoggedIn})} className={`mt-2 px-3 py-1 ${val.isLoggedIn ? "bg-blue-400" : "bg-red-400"} rounded-full text-white`}>Change</button>
-
+    <div className="p-5">
+      {val.map((item) => ( 
+      <div>
+      <h1>{item.name}</h1>
+      <h2>{item.age}</h2>
+      </div>
+      ))}
+      <button onClick={()=>setVal(()=>val.map((item)=>(item.name) === "messi" ? ({name:"messi", age: 37}) : item))} className="px-2 py-1 bg-blue-400 rounded-full text-xs text-white">Click</button>
     </div>
-  
+
   )
 }
 
