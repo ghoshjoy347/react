1f38b7a16f4ebfc56e25b96baf99d2d81a6b959b
Joy Ghosh <145364240+ghoshjoy347@users.noreply.github.com>
2024-02-07 00:14:12 +0000

    routing

---
+++ 11_routing/src/components/User.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function User() {
+  return (
+    <div className='w-1/2 m-auto mt-10'>
+        <h1 className='text-3xl mb-2'>User</h1>
+        <p className='mb-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui dicta inventore dolor, aliquam sit ducimus. Aperiam aspernatur harum impedit? Nostrum?</p>
+        <button className='p-3 bg-green-200'>Explore More</button>
+    </div>
+  )
+}
+
+export default User
---
+++ 11_routing/src/utils/Routing.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../components/Home'
+import User from '../components/User'
+import About from '../components/About'
+
+function Routing() {
+  return (
+    <Routes>
+
+
+  <Route path='/' element={<Home />} />
+  <Route path='/User' element={<User />} />
+  <Route path='/About' element={<About />} />
+
+
+  </Routes>
+  )
+}
+
+export default Routing
